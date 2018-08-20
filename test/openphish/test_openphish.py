23fc27b3c9ae740edc7d5757226127d709aaa122
Wes Young <wes@barely3am.com>
2018-08-20 15:31:21 -0400

    adding abuse_ch tests

--- test/openphish/test_openphish.py
+++ test/openphish/test_openphish.py
@@ -1,5 +1,6 @@
 from csirtg_fm import FM
 from csirtg_fm.rule import Rule
+from csirtg_fm.utils.content import get_type
 
 rule = 'test/openphish/openphish.yml'
 rule = Rule(path=rule)
@@ -12,7 +13,10 @@ def test_openphish():
     from csirtg_fm.clients.http import Client
     cli = Client(rule, 'urls')
 
-    for i in s.process(rule, 'urls', 'pattern', cli, limit=25, indicators=[]):
+    parser_name = get_type(cli.cache)
+    assert parser_name == 'csv'
+
+    for i in s.process(rule, 'urls', parser_name, cli, limit=25, indicators=[]):
         if not i:
             continue
 
